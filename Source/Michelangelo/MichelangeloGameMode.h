571a9f7427729fbfb154c318ca73ce9eec9beb53
jpmmaia <jpmmaia@gmail.com>
2016-10-26 21:31:20 +0200

    - Initializing Unreal Engine project

---
+++ Source/Michelangelo/MichelangeloGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MichelangeloGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MICHELANGELO_API AMichelangeloGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
